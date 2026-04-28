347f92575510cf2e5bd83b245bf88666dbf38ae7
Arindam Saha <73385029+arindamss007@users.noreply.github.com>
2026-04-28 15:58:33 +0530

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.getElementById("menuToggle");
-const navbar = document.getElementById("navbar");
-
-menuToggle.addEventListener("click", () => {
-  navbar.classList.toggle("active");
-});
